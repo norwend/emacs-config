f4584dbfc6bb906a7ff5fd294f817ee078f62f6b
Stepan Ilyin <norwend@tuta.io>
2022-05-04 00:56:19 +0300

    git (magit)

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" "e6f3a4a582ffb5de0471c9b640a5f0212ccf258a987ba421ae2659f1eaa39b09" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "4699e3a86b1863bbc695236036158d175a81f0f3ea504e2b7c71f8f7025e19e3" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "84b14a0a41bb2728568d40c545280dbe7d6891221e7fbe7c2b1c54a3f5959289" "b5803dfb0e4b6b71f309606587dd88651efe0972a5be16ece6a958b197caeed8" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "266ecb1511fa3513ed7992e6cd461756a895dcc5fef2d378f165fed1c894a78c" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "3d47380bf5aa650e7b8e049e7ae54cdada54d0637e7bac39e4cc6afb44e8463b" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "028c226411a386abc7f7a0fba1a2ebfae5fe69e2a816f54898df41a6a3412bb5" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "353ffc8e6b53a91ac87b7e86bebc6796877a0b76ddfc15793e4d7880976132ae" default))
  '(package-selected-packages
-   '(counsel-projectile projectile hydra evil-collection evil general helpful good-scroll smooth-scrolling ivy-rich which-key use-package doom-themes doom-modeline counsel)))
+   '(evil-magit counsel-projectile projectile hydra evil-collection evil general helpful good-scroll smooth-scrolling ivy-rich which-key use-package doom-themes doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -178,7 +178,12 @@
     ; Projectile
     "p"  '(projectile-command-map :which-key "projectile")
 
+    ; Git
+    "g"  '(:ignore t :which-key "git")
+    "gs" '(magit-status :which-key "Git status")
+    
     ; Opening sth
+    "o"  '(:ignore t :which-key "open")
     "ot" '(term :which-key "Open terminal")
     "op" '(treemacs :which-key "Open file bar")
     
@@ -239,6 +244,12 @@
 ;;(define-key projectile-mode-map (kbd "SPC p") 'projectile-command-map)
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+
 (use-package treemacs
   :ensure t
   :defer t
@@ -336,9 +347,9 @@
   :hook (dired-mode . treemacs-icons-dired-enable-once)
   :ensure t)
 
-;(use-package treemacs-magit
-;  :after (treemacs magit)
-;  :ensure t)
+(use-package treemacs-magit
+ :after (treemacs magit)
+ :ensure t)
 
 (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
   :after (treemacs persp-mode) ;;or perspective vs. persp-mode
@@ -349,3 +360,4 @@
   :after (treemacs)
   :ensure t
   :config (treemacs-set-scope-type 'Tabs))
+
